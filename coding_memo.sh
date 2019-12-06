98d30ce0063db8d8a44f12ae3fed4fdb487cc4c9
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2019-12-07 04:38:47 +0900

    updated repository

--- coding_memo.sh
+++ coding_memo.sh
@@ -27,18 +27,18 @@ list_all_commands() {
 
 case $1 in
 	help      ) list_all_commands;; 
-	vsftpd    ) cat ~/desktop/memos/about_vsftpd.txt | grep -A 5 $2;;
-	apache    ) cat ~/desktop/memos/apache2_memo.txt | grep -A 5 $2;;
-	docker    ) cat ~/desktop/memos/docker_about.txt ~/desktop/memos/docker_commands.txt | grep -A 5 $2;;
-	gedit     ) cat ~/desktop/memos/gedit_settings_memo.txt | grep -A 5 $2;;
-	formhelper) cat ~/desktop/memos/formhelper.txt | grep -A 5 $2;;
-	git       ) cat ~/desktop/memos/gitcommand.txt | grep -A 5 $2;;
-	keyboard  ) cat ~/desktop/memos/keyboard_shortcut.txt | grep -A 5 $2;;
-	linux     ) cat ~/desktop/memos/linux_command_memo.txt | grep -A 5 $2;;
-	lynx      ) cat ~/desktop/memos/lynx_memo.txt | grep -A 5 $2;;
+	vsftpd    ) cat ~/desktop/memos/about_vsftpd.txt | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	apache    ) cat ~/desktop/memos/apache2_memo.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	docker    ) cat ~/desktop/memos/docker_about.txt ~/desktop/memos/docker_commands.txt | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	gedit     ) cat ~/desktop/memos/gedit_settings_memo.txt | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	formhelper) cat ~/desktop/memos/formhelper.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	git       ) cat ~/desktop/memos/gitcommand.txt | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	keyboard  ) cat ~/desktop/memos/keyboard_shortcut.txt | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	linux     ) cat ~/desktop/memos/linux_command_memo.txt | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	lynx      ) cat ~/desktop/memos/lynx_memo.txt | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
 	markdown  ) cat ~/desktop/memos/markdown.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	ty        ) cat ~/desktop/memos/pic_and_tyls.txt | grep -A 5 $2;;
-	ruby      ) cat ~/desktop/memos/ruby_command.txt | grep -A 5 $2;;
-	verilog   ) cat ~/desktop/memos/verilog_memo.txt | grep -A 5 $2;;
+	ty        ) cat ~/desktop/memos/pic_and_tyls.txt | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	ruby      ) cat ~/desktop/memos/ruby_command.txt | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	verilog   ) cat ~/desktop/memos/verilog_memo.txt | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
 	*         ) echo -e "ERROR: wrong command!!!\ntype 'sc help' for more information";;
 esac
