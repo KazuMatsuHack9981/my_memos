87bcb6155c581ad240ce2f4526a2ea8b3ff0ede0
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2019-12-07 22:22:03 +0900

    editted gedit_settings_memo.md

--- coding_memo.sh
+++ coding_memo.sh
@@ -34,7 +34,7 @@ case $1 in
 	apache     ) cat ~/desktop/memos/apache2_memo.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
 	docker     ) cat ~/desktop/memos/docker_commands.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
 	dockerabout) cat ~/desktop/memos/docker_about.md | awk "/About/,/\*\*\*/" | sed -r "s/(About)/\x1b[38;5;82m\1\x1b[0m/g";;
-	gedit      ) cat ~/desktop/memos/gedit_settings_memo.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	gedit      ) cat ~/desktop/memos/gedit_settings_memo.md | awk "/About/,/\*\*\*/" | sed -r "s/(About)/\x1b[38;5;82m\1\x1b[0m/g";;
 	formhelper ) cat ~/desktop/memos/formhelper.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
 	git        ) cat ~/desktop/memos/gitcommand.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
 	keyboard   ) cat ~/desktop/memos/keyboard_shortcut.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
