5a6a3ca599311e18fef5ebe52bbed30b9b6f5583
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2019-12-07 14:41:14 +0900

    editted linux_xommand_memo.md

--- coding_memo.sh
+++ coding_memo.sh
@@ -27,18 +27,18 @@ list_all_commands() {
 
 case $1 in
 	help      ) list_all_commands;; 
-	vsftpd    ) cat ~/desktop/memos/about_vsftpd.txt | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	vsftpd    ) cat ~/desktop/memos/about_vsftpd.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
 	apache    ) cat ~/desktop/memos/apache2_memo.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	docker    ) cat ~/desktop/memos/docker_about.txt ~/desktop/memos/docker_commands.txt | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	gedit     ) cat ~/desktop/memos/gedit_settings_memo.txt | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	docker    ) cat ~/desktop/memos/docker_about.md ~/desktop/memos/docker_commands.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	gedit     ) cat ~/desktop/memos/gedit_settings_memo.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
 	formhelper) cat ~/desktop/memos/formhelper.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
 	git       ) cat ~/desktop/memos/gitcommand.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	keyboard  ) cat ~/desktop/memos/keyboard_shortcut.txt | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	linux     ) cat ~/desktop/memos/linux_command_memo.txt | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	lynx      ) cat ~/desktop/memos/lynx_memo.txt | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	keyboard  ) cat ~/desktop/memos/keyboard_shortcut.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	linux     ) cat ~/desktop/memos/linux_command_memo.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	lynx      ) cat ~/desktop/memos/lynx_memo.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
 	markdown  ) cat ~/desktop/memos/markdown.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	ty        ) cat ~/desktop/memos/pic_and_tyls.txt | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	ruby      ) cat ~/desktop/memos/ruby_command.txt | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	verilog   ) cat ~/desktop/memos/verilog_memo.txt | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	ty        ) cat ~/desktop/memos/pic_and_tyls.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	ruby      ) cat ~/desktop/memos/ruby_command.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	verilog   ) cat ~/desktop/memos/verilog_memo.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
 	*         ) echo -e "ERROR: wrong command!!!\ntype 'sc help' for more information";;
 esac
---
+++ linux_command_memo.md
@@ -0,0 +1,131 @@
+# linux command memo
+
+
+## open files
+open files by a defaut application
+```
+xdg-open (filename)
+```
+
+
+
+***
+
+
+
+## apt
+install applications
+```
+sudo apt instal name
+```
+  
+uninstall applications and delete settings files too
+```
+sudo apt purge name
+```
+
+
+
+***
+
+
+
+## groups
+All users are in some kinds of group  
+  
+list all groups you are in
+```
+groups
+```
+  
+show a group which a user "name" are in
+```
+groups name
+```
+
+
+
+***
+
+
+
+## make users
+add the users named "name"
+```
+adduser name
+```
+
+
+
+***
+
+  
+
+## change the password
+change the password of the user "name"
+```
+passwd name
+```
+
+
+
+***
+
+
+
+## add user to group
+add the user named "user" to a group "group"
+```
+usermod -aG group user
+```
+
+
+
+***
+
+
+
+## show user status
+show anything about users named "user"  
+for example, what groups is the user in
+```
+id user
+```
+
+
+
+***
+
+
+
+## make common directory
+make "directory" a common directory in the same group "group"
+```
+chgrp -R group directory
+```
+
+then, you might have to change authority by,
+```
+chmod -R 770 directory : ↑の次にこれをやる。
+```
+
+
+
+***
+
+
+
+
+## portscan
+portscan your own local computer
+```
+nmap localhost
+```
+
+
+
+***
+
+
+
+
