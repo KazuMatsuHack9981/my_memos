a77d6d6b1137afaeff8831134c39d49a3fd6f2f5
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2019-12-07 21:44:41 +0900

    editted docker

--- coding_memo.sh
+++ coding_memo.sh
@@ -11,6 +11,7 @@ list_all_commands() {
 	echo "vsftpd"
 	echo "apache"
 	echo "docker"
+	echo "dockerabout"
 	echo "gedit"
 	echo "formhelper"
 	echo "git"
@@ -27,20 +28,21 @@ list_all_commands() {
 
 
 case $1 in
-	help      ) list_all_commands;; 
-	vsftpd    ) cat ~/desktop/memos/about_vsftpd.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	apache    ) cat ~/desktop/memos/apache2_memo.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	docker    ) cat ~/desktop/memos/docker_about.md ~/desktop/memos/docker_commands.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	gedit     ) cat ~/desktop/memos/gedit_settings_memo.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	formhelper) cat ~/desktop/memos/formhelper.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	git       ) cat ~/desktop/memos/gitcommand.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	keyboard  ) cat ~/desktop/memos/keyboard_shortcut.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	linux     ) cat ~/desktop/memos/linux_command_memo.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	lynx      ) cat ~/desktop/memos/lynx_memo.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	markdown  ) cat ~/desktop/memos/markdown.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	ty        ) cat ~/desktop/memos/pic_and_tyls.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	ruby      ) cat ~/desktop/memos/ruby_command.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	verilog   ) cat ~/desktop/memos/verilog_memo.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	tas       ) cat ~/desktop/memos/tas_memo.md |  awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	*         ) echo -e "ERROR: wrong command!!!\ntype 'sc help' for more information";;
+	help       ) list_all_commands;; 
+	vsftpd     ) cat ~/desktop/memos/about_vsftpd.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	apache     ) cat ~/desktop/memos/apache2_memo.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	docker     ) cat ~/desktop/memos/docker_commands.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	dockerabout) cat ~/desktop/memos/docker_about.md | awk "/About/,/\*\*\*/" | sed -r "s/(About)/\x1b[38;5;82m\1\x1b[0m/g";;
+	gedit      ) cat ~/desktop/memos/gedit_settings_memo.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	formhelper ) cat ~/desktop/memos/formhelper.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	git        ) cat ~/desktop/memos/gitcommand.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	keyboard   ) cat ~/desktop/memos/keyboard_shortcut.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	linux      ) cat ~/desktop/memos/linux_command_memo.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	lynx       ) cat ~/desktop/memos/lynx_memo.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	markdown   ) cat ~/desktop/memos/markdown.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	ty         ) cat ~/desktop/memos/pic_and_tyls.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	ruby       ) cat ~/desktop/memos/ruby_command.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	verilog    ) cat ~/desktop/memos/verilog_memo.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	tas        ) cat ~/desktop/memos/tas_memo.md |  awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	*          ) echo -e "ERROR: wrong command!!!\ntype 'sc help' for more information";;
 esac
