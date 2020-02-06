56ea14aba6cc6f816b203a6b00113020423646b1
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-02-06 23:02:22 +0900

    added rails folder

--- coding_memo.sh
+++ coding_memo.sh
@@ -33,8 +33,7 @@ list_all_commands() {
 	echo "vscode"
 	echo "vimscript"
 	echo "gdb"
-	echo "findrails"
-	echo "validation"
+	echo "rails"
 	echo "api"
 }
 
@@ -67,8 +66,7 @@ case $1 in
 	vscode     ) cat ~/desktop/memos/vscode_commands.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
 	vimscript  ) cat ~/desktop/memos/vimscript.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";; 
 	gdb        ) cat ~/desktop/memos/gdb_about.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	findrails  ) cat ~/desktop/memos/find_rails.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
-	validation ) cat ~/desktop/memos/validation_rails.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
+	rails      ) cat ~/desktop/memos/rails/*.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
 	api        ) cat ~/desktop/memos/apis.md | awk "/$2/,/\*\*\*/" | sed -r "s/($2)/\x1b[38;5;82m\1\x1b[0m/g";;
 	*          ) echo -e "ERROR: wrong command!!!\ntype 'sc help' for more information";;
 esac
